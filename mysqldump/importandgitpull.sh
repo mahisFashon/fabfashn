99cccd2f281cc0e1eaec5139f35d74247ad8d74d
mahisfashionparadise <mahisfashionparadise@gmail.com>
2021-05-07 15:07:22 -0400

    latest change added in git repository

--- mysqldump/importandgitpull.sh
+++ mysqldump/importandgitpull.sh
@@ -3,9 +3,10 @@ echo pull changes
 
 git pull
 cd /var/www/html/fabfashn/mysqldump
-echo import sql db
+
+#echo import sql db
 
 #sed -i 's/localhost/192.168.1.236/g' mahisfashiondb.sql
 
-mysql -f --user=root --password=sh22ee05 fabfashn < fabfashn.sql
+#mysql -f --user=root --password=sh22ee05 fabfashn < fabfashn.sql
 echo Done!
