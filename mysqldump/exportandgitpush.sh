2b6096bd0859ada130980a82ae0ffaa5bb84d597
mahisfashionparadise <mahisfashionparadise@gmail.com>
2021-05-07 11:45:13 -0400

    latest change added in git repository

--- mysqldump/exportandgitpush.sh
+++ mysqldump/exportandgitpush.sh
@@ -8,16 +8,16 @@ echo db dump started
 
 mysqldump --insert-ignore --user=root --password=sh22ee05 fabfashn > fabfashn.sql
 #sed -i 's/192.168.1.236/localhost/g' fabfashn.sql
-sed -i 's/$1/$2/g' fabfashn.sql
+sed -i "s/$1/$2/g" fabfashn.sql
 echo db dump done
 
-cd /var/www/html/fabfashn/
+ct 0 /var/www/html/fabfashn/
 
 echo add updated files
 git add *
 echo commit changes
 
-git commit -a -m "latest change added in  db"
+git commit -a -m "latest change added in git repository"
 
 echo push changes
 
